55fffd776d6f87d167fe43915737da160ebffe24
Abby Xie <60021932+zzbbcsd@users.noreply.github.com>
2022-08-24 12:37:28 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import FeaturedSection from "./components/featuredSection";
-// import ShortStories from "./components/shortStories";
-// import WhatIsMore from "./components/whatIsMore";
 import AboutAuthor from "./components/AboutAuthor";
 import MoreBooks from "./components/MoreBooks";
 import Footer from "./components/footer";
